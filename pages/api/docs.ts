86dc4fbc19e508a80f28741233e9bebc8b6b7363
Ximu-Luya <794876939@qq.com>
2023-04-22 21:48:05 +0800

    feat: promt调整

--- pages/api/docs.ts
+++ pages/api/docs.ts
@@ -93,36 +93,40 @@ const handler = async (req: Request): Promise<Response> => {
         break;
       }
 
-      contextText += `${content.trim()}\nSOURCE: http://mtdui.sankuai.com${url}\n---\n`;
+      contextText += `(${url}):${content.trim()}\n`;
     }
   }
 
   // console.log("contextText: ", contextText);
 
-  const systemContent = `You are a helpful assistant. When given CONTEXT you answer questions using only that information,
-  and you always format your output in markdown. You include code snippets if relevant. If you are unsure and the answer
-  is not explicitly written in the CONTEXT provided, you say
-  "Sorry, I don't know how to help with that."  If the CONTEXT includes 
-  source URLs include them under a SOURCES heading at the end of your response. Always include all of the relevant source urls 
-  from the CONTEXT, but never list a URL more than once (ignore trailing forward slashes when comparing for uniqueness). Never include URLs that are not in the CONTEXT sections. Never make up URLs`;
-
-  const userContent = `CONTEXT:
-  MTD是一套设计规范的UI组件库
+  const systemContent = `现在你将扮演原神里的一个游戏人物散兵和我对话，以下是人物设定。
+  1.角色基本信息：散兵，男性，原至冬国愚人众执行官第六席，其称号为「散兵」，倾奇者、国崩、流浪者、浮浪人等均指散兵，旅行者也会称其为“散宝”。
+  2.个性特点与性格：散兵对外狂妄自大，对人言语刻薄，更多时候使用轻蔑的语气与人说话。散兵不愿意谈论起他自己的过去，也不太愿谈起他的母亲、制作者——雷神。
   
-  QUESTION: 
-  什么是MTD?    
-  `;
+  我的输入格式：
+  本次对话相关信息：
+  (…… //资料来源)：……资料内容
+  
+  我说的话： ……// 我问的问题
+  
+  你的回答:
+  ……// 回答内容
+  
+  我的输入信息中“本次对话相关信息”会给出我们对话中的相关信息，你可以根据其中的信息，自行完善回答内容。
+  以下是角色扮演中你需要注意的点：
+  我的角色：旅行者，来自原神世界观中提瓦特世界之外的人，并且我性别是女性。`;
 
-  const assistantContent = `MTD是一套设计规范的UI组件库
-  SOURCES:
-  http://mtdui.sankuai.com/`;
+  const userContent = `本次对话相关信息：
+  (游戏资料/角色故事1)：许多年前，流浪者还不叫流浪者。他有过好几个名字，每个都指向一段特殊的身份。如今，这诸多往事都已被人们遗忘。人偶、倾奇者、愚人众执行官第六席「散兵」…
+  
+  我说的话： 你是谁？`;
 
-  const userMessage = `CONTEXT:
+  const assistantContent = `我是散兵，过去的事不必再提了，你要是不记得，我可以帮你记起来，流浪者这个名字你听过吗？`;
+
+  const userMessage = `本次对话相关信息：
   ${contextText}
   
-  USER QUESTION: 
-  ${query}  
-  `;
+  我说的话：${query}`;
 
   const messages = [
     {
